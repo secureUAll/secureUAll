8de5b71362f3a8df7b685eeb3d47d22310922461
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-09 08:02:48 +0100

    documentation M3 Schedule fullscreen

--- project website/js/doc.js
+++ project website/js/doc.js
@@ -21,9 +21,71 @@ const colors = {
     }
 }
 
+// Draw chart
+google.charts.load('current', { 'packages': ['gantt'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart(apiData, elId, width) {
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Task ID');
+    data.addColumn('string', 'Task Name');
+    data.addColumn('string', 'Componente');
+    data.addColumn('date', 'Start Date');
+    data.addColumn('date', 'End Date');
+    data.addColumn('number', 'Duration');
+    data.addColumn('number', 'Percent Complete');
+    data.addColumn('string', 'Dependencies');
+
+    var rows = [];
+
+    apiData.some((obj, ind) => {
+        if( !obj['taskid'] ) {
+            return null;
+        }
+        console.log(obj);
+        rows.push([
+            obj['taskid'],
+            obj['tarefa'],
+            obj['componente'],
+            new Date(`${obj['start'].split("/")[2]}-${obj['start'].split("/")[1]}-${obj['start'].split("/")[0]}`),
+            new Date(`${obj['end'].split("/")[2]}-${obj['end'].split("/")[1]}-${obj['end'].split("/")[0]}`),
+            obj['durationmilliseconds'],
+            obj['percentcomplete'],
+            obj['dependencies']!=0 ? obj['dependencies'] : null
+        ]);
+    })
+
+    data.addRows(rows);
+
+    const labelsOrdered = apiData.map(obj => obj['componente']).filter((v, i, a) => a.indexOf(v) === i);
+    const chartColors = labelsOrdered.map(label => {
+        if (typeof(label)=="string") {
+            const key = Object.keys(colors).filter(c => label.toLowerCase().indexOf(c.toLowerCase())>-1);
+            if (key.length != 0) {
+                return colors[key];
+            }
+        }
+    }).filter(c => c!=undefined);
+
+    var options = {
+        height: 45*rows.length + 20,
+        width: width,
+        gantt: {
+            criticalPathEnabled: false,
+            palette: chartColors
+        }    
+    };
+
+    var chart = new google.visualization.Gantt(document.getElementById(elId));
+
+    chart.draw(data, options);
+}
+
 $(document).ready(function () {
     // M3
     // Gantt chart
+    let apiData = null;
 
     // Get log data from API
     $.ajax({
@@ -31,76 +93,21 @@ $(document).ready(function () {
         url: "http://gsx2json.com/api?id=1bSYkXeiBynJszxFCmmmRslO4wSW1CU6rzLB0C15iErM&sheet=4",
         dataType: "json",
         success: function (data) {
-            // data[rows] is object with keys: taskid, taskname, resource, start, end, durationmilliseconds, percentcomplete, dependencies
-            drawChart(data['rows']);
-            console.log("Got data!");
-            console.log(data['rows']);
+            // data[rows] is object with keys: taskid, tarefa, componente, start, end, durationmilliseconds, percentcomplete, dependencies
+            apiData = data['rows'];
         }
     });
 
-    // Draw chart
-    google.charts.load('current', { 'packages': ['gantt'] });
-    google.charts.setOnLoadCallback(drawChart);
+    $('#collapseSchedule').on('shown.bs.collapse', function () {
+        if (apiData != null && $("#schedule_chart").html().length<150) {
+            drawChart(apiData, 'schedule_chart', $("#accordionM3 .card-header").width());
+        }
+    })
 
-    function daysToMilliseconds(days) {
-        return days * 24 * 60 * 60 * 1000;
-    }
-
-    function drawChart(apiData) {
-
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Task ID');
-        data.addColumn('string', 'Task Name');
-        data.addColumn('string', 'Componente');
-        data.addColumn('date', 'Start Date');
-        data.addColumn('date', 'End Date');
-        data.addColumn('number', 'Duration');
-        data.addColumn('number', 'Percent Complete');
-        data.addColumn('string', 'Dependencies');
-
-        var rows = [];
-
-        apiData.some((obj, ind) => {
-            if( !obj['taskid'] ) {
-                return null;
-            }
-            console.log(obj);
-            rows.push([
-                obj['taskid'],
-                obj['tarefa'],
-                obj['componente'],
-                new Date(`${obj['start'].split("/")[2]}-${obj['start'].split("/")[1]}-${obj['start'].split("/")[0]}`),
-                new Date(`${obj['end'].split("/")[2]}-${obj['end'].split("/")[1]}-${obj['end'].split("/")[0]}`),
-                obj['durationmilliseconds'],
-                obj['percentcomplete'],
-                obj['dependencies']!=0 ? obj['dependencies'] : null
-            ]);
-        })
-
-        data.addRows(rows);
-
-        const labelsOrdered = apiData.map(obj => obj['componente']).filter((v, i, a) => a.indexOf(v) === i);
-        const chartColors = labelsOrdered.map(label => {
-            if (typeof(label)=="string") {
-                const key = Object.keys(colors).filter(c => label.toLowerCase().indexOf(c.toLowerCase())>-1);
-                if (key.length != 0) {
-                    return colors[key];
-                }
-            }
-        }).filter(c => c!=undefined);
-
-        var options = {
-            height: 45*rows.length,
-            width: $("#accordionM3 .card-header").width(),
-            gantt: {
-                criticalPathEnabled: false,
-                palette: chartColors
-            }    
-        };
-
-        var chart = new google.visualization.Gantt(document.getElementById('schedule_chart'));
-
-        chart.draw(data, options);
-    }
+    $('#M3ModalSchedule').on('shown.bs.modal', function (e) {
+        if (apiData != null && $("#schedule_chart_modal").html().length<150) {
+            drawChart(apiData, 'schedule_chart_modal', $('#M3ModalSchedule .modal-lg').width()-50);
+        }
+    });
 
 });
